a28b75fc0c68cae27205b1ae62f352934a3c83d3
Jon LaBelle <contact@jonlabelle.com>
2018-11-09 07:47:36 -0600

    A better windows build command

--- build.cmd
+++ build.cmd
@@ -1,3 +1,17 @@
-rd /s /q build
+@echo off
+setlocal
+
+set PROJECT_ROOT="%~dp0"
+
+pushd %PROJECT_ROOT%
+
+if exist "build" (rmdir /s /q "build")
+if exist "secure-file\bin" (rmdir "secure-file\bin" /s /q)
+if exist "secure-file\obj" (rmdir "secure-file\obj" /s /q)
+
+dotnet restore
 dotnet publish secure-file -f netcoreapp2.0 -o ..\build -c Release
 dotnet publish secure-file -f net462 -o ..\build -c Release
+
+endlocal
+@exit /b %errorlevel%
