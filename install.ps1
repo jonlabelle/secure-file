c0c116d4357ae24adbcb8bc1282eaff6637a9d96
Jon LaBelle <contact@jonlabelle.com>
2021-07-14 03:01:34 -0500

    Remove Travis configs

--- install.ps1
+++ install.ps1
@@ -8,8 +8,8 @@ if ($isLinux) {
 $zipPath = Join-Path $temp 'secure-file.zip'
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 (New-Object Net.WebClient).DownloadFile('https://github.com/jonlabelle/secure-file/archive/master.zip', $zipPath)
-Expand-Archive $zipPath -DestinationPath (Join-Path (pwd).path "appveyor-tools")
+Expand-Archive $zipPath -DestinationPath (Join-Path (Get-Location).path "appveyor-tools")
 if ($isLinux) {
 	chmod +x ./appveyor-tools/secure-file
 }
-del $zipPath
+Remove-Item $zipPath
